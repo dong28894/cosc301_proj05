00aa5cd345078442e4e8d29869e304676aa71a3a
Dong Mai <dmai@colgate.edu>
2014-12-11 02:03:51 -0500

    proj05 finished

--- scandisk.c
+++ scandisk.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "bootsect.h"
 #include "bpb.h"
@@ -14,23 +15,105 @@
 #include "fat.h"
 #include "dos.h"
 
-int check_file_inconsistency(struct direntry *dirent, uint8_t *image_buf, struct bpb33* bpb){
+void write_dirent(struct direntry *dirent, char *filename, 
+		  uint16_t start_cluster, bool *refered_sectors,
+			uint8_t *image_buf, struct bpb33* bpb){
+	int size = 0;
+    /* clean out anything old that used to be here */
+    memset(dirent, 0, sizeof(struct direntry));    
+
+    /* set the file name and extension */
+    memset(dirent->deName, ' ', 8);
+    memcpy(dirent->deExtension, "dat", 3);    
+
+    if (strlen(filename)>8){
+		filename[8]='\0';
+    }
+    memcpy(dirent->deName, filename, strlen(filename));
+
+    /* set the attributes and file size */
+    dirent->deAttributes = ATTR_NORMAL;
+    putushort(dirent->deStartCluster, start_cluster);
+	uint16_t cluster = start_cluster;
+	while (is_valid_cluster(cluster, bpb)){
+		size++;
+		refered_sectors[cluster] = true;		
+		cluster = get_fat_entry(cluster, image_buf, bpb);		
+	}
+    putulong(dirent->deFileSize, size*bpb->bpbBytesPerSec);
+}
+
+
+void create_dirent(struct direntry *dirent, char *filename, 
+		   uint16_t start_cluster, bool *refered_sectors,
+			uint8_t *image_buf, struct bpb33* bpb){
+    while (1){
+		if (dirent->deName[0] == SLOT_EMPTY){
+	    	/* we found an empty slot at the end of the directory */
+	    	write_dirent(dirent, filename, start_cluster, refered_sectors, image_buf, bpb);
+	    	dirent++;
+
+	    	/* make sure the next dirent is set to be empty, just in
+	       case it wasn't before */
+	    	memset((uint8_t*)dirent, 0, sizeof(struct direntry));
+	    	dirent->deName[0] = SLOT_EMPTY;
+	    	return;
+		}
+
+		if (dirent->deName[0] == SLOT_DELETED){
+	    	/* we found a deleted entry - we can just overwrite it */
+	    	write_dirent(dirent, filename, start_cluster, refered_sectors, image_buf, bpb);
+	    	return;
+		}
+		dirent++;
+    }
+}
+
+void find_orphaned(uint8_t *image_buf, struct bpb33* bpb, bool *refered_sectors){
+	int deIndex = 1;
+	char filename[15] = "found";
+	char buffer[10];
+	struct direntry *dirent = (struct direntry*)root_dir_addr(image_buf, bpb);
+	for (int i = CLUST_FIRST; i < bpb->bpbSectors; i++){
+		if (refered_sectors[i] == false && get_fat_entry(i, image_buf, bpb) != CLUST_FREE){
+			printf("%d\n", i);
+			sprintf(buffer, "%d", deIndex);
+			strcat(filename, buffer);
+			create_dirent(dirent, filename, i, refered_sectors, image_buf, bpb);
+			filename[5] = '\0';
+			deIndex++;
+		}
+	}
+}
+
+int check_file_inconsistency(struct direntry *dirent, uint8_t *image_buf, struct bpb33* bpb, bool *refered_sectors){
 	uint32_t size = getulong(dirent->deFileSize);
 	uint16_t cluster = getushort(dirent->deStartCluster);
 	uint16_t cluster_size = bpb->bpbBytesPerSec * bpb->bpbSecPerClust;
 	int numClusters = (size + cluster_size - 1)/cluster_size;
 	int realNumClusters = 0;
 	while (is_valid_cluster(cluster, bpb)){
-		realNumClusters++;		
-		cluster = get_fat_entry(cluster, image_buf, bpb);		
+		realNumClusters++;
+		refered_sectors[cluster] = true;		
+		uint16_t next_cluster = get_fat_entry(cluster, image_buf, bpb);
+		if (next_cluster != cluster){
+			cluster = next_cluster;
+		}else{
+			set_fat_entry(cluster, FAT12_MASK&CLUST_BAD, image_buf, bpb);
+			printf("metadata size: %d\n", numClusters);
+			printf("Real size: %d\n", realNumClusters);
+			if (numClusters <= realNumClusters){			
+				return -1;
+			}else if (numClusters > realNumClusters){
+				return 1;
+			}
+		}		
 	}
-	if (numClusters < realNumClusters){
-		printf("metadata size: %d\n", numClusters);
-		printf("Real size: %d\n", realNumClusters);	
+	printf("metadata size: %d\n", numClusters);
+	printf("Real size: %d\n", realNumClusters);
+	if (numClusters < realNumClusters){			
 		return -1;
 	}else if (numClusters > realNumClusters){
-		printf("metadata size: %d\n", numClusters);
-		printf("Real size: %d\n", realNumClusters);
 		return 1;
 	}else{
 		return 0;
@@ -63,7 +146,7 @@ void fix_file_inconsistency(struct direntry *dirent, uint8_t *image_buf, struct 
 	}
 }
 
-uint16_t check_dirent(struct direntry *dirent, uint8_t *image_buf, struct bpb33* bpb){
+uint16_t check_dirent(struct direntry *dirent, uint8_t *image_buf, struct bpb33* bpb, bool *refered_sectors){
     uint16_t followclust = 0;
 
     int i;
@@ -117,40 +200,41 @@ uint16_t check_dirent(struct direntry *dirent, uint8_t *image_buf, struct bpb33*
 		if ((dirent->deAttributes & ATTR_HIDDEN) != ATTR_HIDDEN){	    
             file_cluster = getushort(dirent->deStartCluster);
             followclust = file_cluster;
+			refered_sectors[followclust] = true;
         }
     }
     else{
         /*
          * a "regular" file entry
-         * print file name if there's an inconsistency
          */
-		if (check_file_inconsistency(dirent, image_buf, bpb)){
-			printf("%s.%s\n", name, extension);
+		printf("%s.%s\n", name, extension);
+		if (check_file_inconsistency(dirent, image_buf, bpb, refered_sectors)){
+			printf("Inconsistent\n");
 			fix_file_inconsistency(dirent, image_buf, bpb);
-			check_file_inconsistency(dirent, image_buf, bpb);
+			//check_file_inconsistency(dirent, image_buf, bpb);
 		}
     }
 
     return followclust;
 }
 
-void follow_dir(uint16_t cluster, uint8_t *image_buf, struct bpb33* bpb){
+void follow_dir(uint16_t cluster, uint8_t *image_buf, struct bpb33* bpb, bool *refered_sectors){
     while (is_valid_cluster(cluster, bpb)){
         struct direntry *dirent = (struct direntry*)cluster_to_addr(cluster, image_buf, bpb);
 
         int numDirEntries = (bpb->bpbBytesPerSec * bpb->bpbSecPerClust) / sizeof(struct direntry);
         int i = 0;
 		for ( ; i < numDirEntries; i++){            
-            uint16_t followclust = check_dirent(dirent, image_buf, bpb);
+            uint16_t followclust = check_dirent(dirent, image_buf, bpb, refered_sectors);
             if (followclust)
-                follow_dir(followclust, image_buf, bpb);
+                follow_dir(followclust, image_buf, bpb, refered_sectors);
             dirent++;
 		}
 		cluster = get_fat_entry(cluster, image_buf, bpb);
     }
 }
 
-void traverse_root(uint8_t *image_buf, struct bpb33* bpb)
+void traverse_root(uint8_t *image_buf, struct bpb33* bpb, bool *refered_sectors)
 {
     uint16_t cluster = 0;
 
@@ -159,9 +243,9 @@ void traverse_root(uint8_t *image_buf, struct bpb33* bpb)
     int i = 0;
     for ( ; i < bpb->bpbRootDirEnts; i++)
     {
-        uint16_t followclust = check_dirent(dirent, image_buf, bpb);
+        uint16_t followclust = check_dirent(dirent, image_buf, bpb, refered_sectors);
         if (is_valid_cluster(followclust, bpb))
-            follow_dir(followclust, image_buf, bpb);
+            follow_dir(followclust, image_buf, bpb, refered_sectors);
 
         dirent++;
     }
@@ -186,9 +270,12 @@ int main(int argc, char** argv) {
     bpb = check_bootsector(image_buf);
 
     // your code should start here...
-
-	traverse_root(image_buf, bpb);
-
+	bool refered_sectors[bpb->bpbSectors];
+	for (int i = 0; i < bpb->bpbSectors; i++){
+		refered_sectors[i] = false;	
+	}
+	traverse_root(image_buf, bpb, &refered_sectors[0]);
+	find_orphaned(image_buf, bpb, &refered_sectors[0]);
 
 
 
